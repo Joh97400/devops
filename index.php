7a72946a766e456340c8fbe4ab3b4565ff07f6f9
Joh97400 <grosset.johan@gmail.com>
2016-08-29 14:46:35 +0200

    Fichier de base

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 </script>
  <style type="text/css">
 	body{font-family:"Roboto","Helvetica Neue",Helvetica,Arial,sans-serif;font-size:13px;line-height:1.846;
-	color:#666666;background-color:<?php $couleur ="pink";echo $couleur;?>}
+	color:#666666;background-color:<?php $couleur ="white";echo $couleur;?>}
  </style>
   </head>
   <body>
